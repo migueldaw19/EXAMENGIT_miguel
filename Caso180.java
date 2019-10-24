8c2fb2dcfd2558da99ed094a5ff6cda8ac1905b6
Sergio <55553719+sermo18@users.noreply.github.com>
2019-10-24 20:36:29 +0200

    Update Caso180.java

--- Caso180.java
+++ Caso180.java
@@ -1,53 +1,52 @@
 import java.util.Scanner;
 
-
-public class Caso180 {
+public class Triangulos {
 
 	public static void main(String[] args) {
-			Scanner sc = new Scanner(System.in);
-				
-				int casos = sc.nextInt();
-				int numero;
-				
-				for (int i = 0; i < casos; i++){
-					
-					int a = sc.nextInt();
-					int b = sc.nextInt();
-					int c = sc.nextInt();
-					
-				if(a+b>c) {
-					if (c>=a) {
+		Scanner sc = new Scanner(System.in);
+
+		int casos = sc.nextInt();
+		int numero;
+		int i =0;
+/*
+		for (int i = 0; i < casos; i++){
+		*/
+		while (i<casos) {
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+
+			if(a+b>c) {
+				if (c>=a) {
 					if (Math.pow(c, 2)<Math.pow(a, 2)+Math.pow(b, 2)){
 						System.out.println("ACUTANGULO");
-					}
-					if (Math.pow(c, 2)==Math.pow(a, 2)+Math.pow(b, 2)){
+					}else if (Math.pow(c, 2)==Math.pow(a, 2)+Math.pow(b, 2)){
 						System.out.println("RECTANGULO");
-					}
-					if (Math.pow(c, 2)>Math.pow(a, 2)+Math.pow(b, 2)){
+					}else if (Math.pow(c, 2)>Math.pow(a, 2)+Math.pow(b, 2)){
 						System.out.println("OBTUSANGULO");
 					}
 					
-			}
-					
-					if (a>=c) {
-						if (Math.pow(a, 2)<Math.pow(c, 2)+Math.pow(b, 2)){
-							System.out.println("ACUTANGULO");
-						}
-						if (Math.pow(a, 2)==Math.pow(c, 2)+Math.pow(b, 2)){
-							System.out.println("RECTANGULO");
-						}
-						if (Math.pow(a, 2)>Math.pow(c, 2)+Math.pow(b, 2)){
-							System.out.println("OBTUSANGULO");
-						}
-						
-			}
-					
-						}
-				if(a+b<=c) {
-					System.out.println("IMPOSIBLE");
+				}else if (a>=c) {
+					if (Math.pow(a, 2)<Math.pow(c, 2)+Math.pow(b, 2)){
+						System.out.println("ACUTANGULO");
+					}else if (Math.pow(a, 2)==Math.pow(c, 2)+Math.pow(b, 2)){
+						System.out.println("RECTANGULO");
+					}else if (Math.pow(a, 2)>Math.pow(c, 2)+Math.pow(b, 2)){
+						System.out.println("OBTUSANGULO");
+					}
+	
 				}
+			if(a+b<=c) {
+				System.out.println("IMPOSIBLE");
+				
 			}
+			i++;
+		}
 
 	}
+	}
+
+
 }
+
 
